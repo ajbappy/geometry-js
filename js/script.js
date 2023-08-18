1051a7d231d84d0efe89e34e374f1774b4e7283c
ajbappy <ajbappywahid22@gmail.com>
2023-08-19 00:35:02 +0600

    added all cards

--- js/script.js
+++ js/script.js
@@ -37,11 +37,46 @@ const ractengleBtnCalulate = document
 
 // "Parallelogram-btn
 
-const ParallelogramBtnCalulate = document
+const parallelogramBtnCalulate = document
   .getElementById("Parallelogram-btn")
   .addEventListener("click", function () {
     const firstinputValue = getInputValue("p-f-input");
     const secondInputValue = getInputValue("p-s-input");
     const areaCalculate = firstinputValue * secondInputValue;
     const setArea = setAreaElement("set-Parallelogram-area", areaCalculate);
+  });
+
+// "Parallelogram-btn
+
+const rhombuBtnCalulate = document
+  .getElementById("Rhombus-btn")
+  .addEventListener("click", function () {
+    const firstinputValue = getInputValue("Rhombus-f-input");
+    const secondInputValue = getInputValue("Rhombus-s-input");
+    const areaCalculate = 0.5 * firstinputValue * secondInputValue;
+    const setArea = setAreaElement("set-Rhombus-area", areaCalculate);
+  });
+// Pentagon btn
+
+const pentagonBtnCalulate = document
+  .getElementById("Pentagon-btn")
+  .addEventListener("click", function () {
+    const firstinputValue = getInputValue("Pentagon-f-input");
+    const secondInputValue = getInputValue("Pentagon-s-input");
+    const areaCalculate = 0.5 * firstinputValue * secondInputValue;
+    const setArea = setAreaElement("set-Pentagon-area", areaCalculate);
+  });
+
+// Pentagon btn
+
+const ellipseBtnCalulate = document
+  .getElementById("ellipse-btn")
+  .addEventListener("click", function () {
+    const firstinputValue = getInputValue("ellipse-f-input");
+    const secondInputValue = getInputValue("ellipse-s-input");
+    const areaCalculate = 3.1416 * firstinputValue * secondInputValue;
+    const setArea = setAreaElement(
+      "set-ellipse-area",
+      areaCalculate.toFixed(2)
+    );
   });
